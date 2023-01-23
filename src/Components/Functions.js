592d22f3920f357c701d2c94282f321f1cd63eee
Harpreetpreet <preetx025@gmail.com>
2023-01-23 11:53:48 +0530

    add useState

--- src/Components/Functions.js
+++ src/Components/Functions.js
@@ -1,106 +1,96 @@
-import React from 'react'
+import React, { useState } from 'react';
+
+function Tab({ title, onClick }) {
+  return (
+    <li className="nav-item flex-auto text-center" role="presentation">
+      <a
+        onClick={onClick}
+        className="cursor-pointer nav-link w-full block font-medium text-xs leading-tight uppercase border-x-0 border-t-0 border-b-2 border-transparent px-6
+    
+       py-3 my-2 hover:border-transparent hover:bg-gray-100 focus:border-transparent active"
+        id="tabs-home-tabFill"
+        data-bs-toggle="pill"
+        data-bs-target="#tabs-homeFill"
+        role="tab"
+        aria-controls="tabs-homeFill"
+        aria-selected="true"
+      >
+        {title}
+      </a>
+    </li>
+  );
+}
 
 export default function Functions() {
+  const [activeTab, setActiveTab] = useState('communication');
   return (
     <section className="bg-slate-200">
-    <div className="container mx-auto text-center md:pt-20 pt-10 md:px-16 px-4">
-        
-      <h3>
-        FEATURES
-      </h3>
-      <h1 className="md:text-5xl text-3xl text-purple-900 py-4">
-        Do more with our app
-      </h1>
-      <div className="bg-white">
-      <ul className="nav nav-tabs flex flex-col md:flex-row flex-wrap list-none border-b-0 pl-0 mb-4" id="tabs-tabFill"
-  role="tablist">
-  <li className="nav-item flex-auto text-center" role="presentation">
-    <a href="#tabs-homeFill" className="
-      nav-link
-      w-full
-      block
-      font-medium
-      text-xs
-      leading-tight
-      uppercase
-      border-x-0 border-t-0 border-b-2 border-transparent
-      px-6
-      py-3
-      my-2
-      hover:border-transparent hover:bg-gray-100
-      focus:border-transparent
-      active
-    " id="tabs-home-tabFill" data-bs-toggle="pill" data-bs-target="#tabs-homeFill" role="tab"
-      aria-controls="tabs-homeFill" aria-selected="true">Communication</a>
-  </li>
-  <li className="nav-item flex-auto text-center" role="presentation">
-    <a href="#tabs-profileFill" className="
-      nav-link
-      w-full
-      block
-      font-medium
-      text-xs
-      leading-tight
-      uppercase
-      border-x-0 border-t-0 border-b-2 border-transparent
-      px-6
-      py-3
-      my-2
-      hover:border-transparent hover:bg-gray-100
-      focus:border-transparent
-    " id="tabs-profile-tabFill" data-bs-toggle="pill" data-bs-target="#tabs-profileFill" role="tab"
-      aria-controls="tabs-profileFill" aria-selected="false">Scheduling</a>
-  </li>
-  <li className="nav-item flex-auto text-center" role="presentation">
-    <a href="#tabs-messagesFill" className="
-      nav-link
-      w-full
-      block
-      font-medium
-      text-xs
-      leading-tight
-      uppercase
-      border-x-0 border-t-0 border-b-2 border-transparent
-      px-6
-      py-3
-      my-2
-      hover:border-transparent hover:bg-gray-100
-      focus:border-transparent
-    " id="tabs-messages-tabFill" data-bs-toggle="pill" data-bs-target="#tabs-messagesFill" role="tab"
-      aria-controls="tabs-messagesFill" aria-selected="false">Messages</a>
-  </li>
-  <li className="nav-item flex-auto text-center" role="presentation">
-    <a href="#tabs-profileFill" className="
-      nav-link
-      w-full
-      block
-      font-medium
-      text-xs
-      leading-tight
-      uppercase
-      border-x-0 border-t-0 border-b-2 border-transparent
-      px-6
-      py-3
-      my-2
-      hover:border-transparent hover:bg-gray-100
-      focus:border-transparent
-    " id="tabs-profile-tabFill" data-bs-toggle="pill" data-bs-target="#tabs-profileFill" role="tab"
-      aria-controls="tabs-profileFill" aria-selected="false">Live Chat</a>
-  </li>
-</ul>
-<div className="tab-content" id="tabs-tabContentFill">
-  <div className="tab-pane fade show active" id="tabs-homeFill" role="tabpanel" aria-labelledby="tabs-home-tabFill">
-    Tab 1 content fill
-  </div>
-  <div className="tab-pane fade" id="tabs-profileFill" role="tabpanel" aria-labelledby="tabs-profile-tabFill">
-    Tab 2 content fill
-  </div>
-  <div className="tab-pane fade" id="tabs-messagesFill" role="tabpanel" aria-labelledby="tabs-profile-tabFill">
-    Tab 3 content fill
-  </div>
-</div>
-</div>
-    </div>
+      <div className="container mx-auto text-center md:pt-20 pt-10 md:px-16 px-4">
+        <h3>FEATURES</h3>
+        <h1 className="md:text-5xl text-3xl text-purple-900 py-4">
+          Do more with our app
+        </h1>
+        <div className="bg-white">
+          <ul
+            className="nav nav-tabs flex flex-col md:flex-row flex-wrap list-none border-b-0 pl-0 mb-4"
+            id="tabs-tabFill"
+            role="tablist"
+          >
+            <Tab
+              title="Communication"
+              onClick={() => setActiveTab('communication')}
+            />
+            <Tab
+              title="Scheduling"
+              onClick={() => setActiveTab('scheduling')}
+            />
+            <Tab title="Messages" onClick={() => setActiveTab('messages')} />
+            <Tab title="Live chat" onClick={() => setActiveTab('liveChat')} />
+          </ul>
+          <div className="tab-content" id="tabs-tabContentFill">
+            {activeTab === 'communication' && (
+              <div
+                className="tab-pane fade show active"
+                id="tabs-homeFill"
+                role="tabpanel"
+                aria-labelledby="tabs-home-tabFill"
+              >
+                communication hfjyhrf hgjer
+              </div>
+            )}
+            {activeTab === 'scheduling' && (
+              <div
+                className="tab-pane fade show active"
+                id="tabs-homeFill"
+                role="tabpanel"
+                aria-labelledby="tabs-home-tabFill"
+              >
+                scheduling hfjyhrf hgjer
+              </div>
+            )}
+            {activeTab === 'messages' && (
+              <div
+                className="tab-pane fade show active"
+                id="tabs-homeFill"
+                role="tabpanel"
+                aria-labelledby="tabs-home-tabFill"
+              >
+                messages hfjyhrf hgjer
+              </div>
+            )}
+            {activeTab === 'liveChat' && (
+              <div
+                className="tab-pane fade show active"
+                id="tabs-homeFill"
+                role="tabpanel"
+                aria-labelledby="tabs-home-tabFill"
+              >
+                live chat etc hfjyhrf hgjer
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
     </section>
-  )
+  );
 }
