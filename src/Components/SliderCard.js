dda639477d61c0673746de7b500b0045e2a41509
Harpreetpreet <preetx025@gmail.com>
2023-01-24 12:42:48 +0530

    fix slider

---
+++ src/Components/SliderCard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const SliderCard = () => {
+  return (
+    <div className=' container mx-auto'>
+
+<h1 className=' text-lg text-center text-red-700'>ewfwkfkef</h1>
+<h1 className=' text-lg text-center text-red-700'>ewfwkfkef</h1><h1 className=' text-lg text-center text-red-700'>ewfwkfkef</h1><h1 className=' text-lg text-center text-red-700'>ewfwkfkef</h1>
+
+      
+    </div>
+  )
+}
+
+export default SliderCard
--- src/Components/Testimonials.js
+++ src/Components/Testimonials.js
@@ -1,16 +1,46 @@
-import Image from 'next/image'
-import React from 'react'
-import ComponentHeader from './global/ComponentHeader'
+import Image from "next/image";
+import React from "react";
+import Slider from "react-slick";
+import Cardcontent from "./Cardcontent";
+import ComponentHeader from "./global/ComponentHeader";
+import SliderCard from "./SliderCard";
 
-const Testimonials = () => {
-  return (
-    <section className="py-12">
-    <div className="container mx-auto">
-      <ComponentHeader heading="Testimonials" title="What our customers says"/>
-      <Image src="/images/" alt=''
-    </div>
-    </section>
-  )
+
+
+export default function Testimonials() {
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+    };
+    return (
+        <div className="container mx-auto md:py-20 py-14">
+            <ComponentHeader heading="Testimonials" title="What our customers says"/> 
+      <div className="px-16 mx-auto py-16 ">
+        <Slider {...settings}>
+          <div >
+            <Image className="rounded-full" src="/images/client.png.webp" alt="" width={100} height={100} />
+            <p>Uniquely streamline highly efficient scenarios and 24/7 initiatives. Conveniently embrace multifunctional ideas through proactive customer service. Distinctively conceptualize 2.0 intellectual capital via user-centric partnerships.</p>
+            <h1>Crystal Gordon</h1>
+            <h3>United states</h3>
+          </div>
+          <div >
+          <Image className="rounded-full" src="/images/client.png.webp" alt="" width={100} height={100} />
+            <p>Uniquely streamline highly efficient scenarios and 24/7 initiatives. Conveniently embrace multifunctional ideas through proactive customer service. Distinctively conceptualize 2.0 intellectual capital via user-centric partnerships.</p>
+            <h1>Crystal Gordon</h1>
+            <h3>United states</h3>
+          </div>
+          <div>
+          <Image className="rounded-full" src="/images/client.png.webp" alt="" width={100} height={100} />
+            <p>Uniquely streamline highly efficient scenarios and 24/7 initiatives. Conveniently embrace multifunctional ideas through proactive customer service. Distinctively conceptualize 2.0 intellectual capital via user-centric partnerships.</p>
+            <h1>Crystal Gordon</h1>
+            <h3>United states</h3>
+          </div>
+        </Slider>
+      </div>
+  </div>
+    );
+  
 }
-
-export default Testimonials
