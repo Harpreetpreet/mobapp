2e83c4a1aa40bbf91de04e00e0e85aa0d8cd0f8c
Harpreetpreet <preetx025@gmail.com>
2023-01-31 12:07:56 +0530

    add columns in pricing components

--- src/Components/Pricing.js
+++ src/Components/Pricing.js
@@ -1,14 +1,55 @@
-import React from 'react'
-import ComponentHeader from './global/ComponentHeader'
+import React from 'react';
+import Button from './Button';
+import ComponentHeader from './global/ComponentHeader';
 
 const Pricing = () => {
   return (
     <section className="py-10">
-    <div className="container mx-auto ">
-      <ComponentHeader heading="PRICING" title="Upgrade to Pro"/>
-    </div>
+      <div className="container mx-auto ">
+        <ComponentHeader heading="PRICING" title="Upgrade to Pro" />
+        <ul className="my-14">
+          <div class="grid md:grid-cols-3 grid-cols-1 gap-6 text-center">
+            <div className="rounded border-2 py-8">
+              <h3>PERSONAL</h3>
+              <h1 className="text-4xl pb-10">$14/m</h1>
+              <div className="space-y-10">
+                <li>10 Projects</li>
+                <li>5 GB Storage</li>
+                <li>Basic Support</li>
+                <li>Collaboration</li>
+                <li>Reports and analytics</li>
+                <Button> CHOOSE THIS PLAN </Button>
+              </div>
+            </div>
+            <div className="rounded border-2 py-8">
+              <h3>PERSONAL</h3>
+              <h1 className="text-4xl pb-10">$14/m</h1>
+              <div className="space-y-10">
+                <li>10 Projects</li>
+                <li>5 GB Storage</li>
+                <li>Basic Support</li>
+                <li>Collaboration</li>
+                <li>Reports and analytics</li>
+                <Button> CHOOSE THIS PLAN </Button>
+              </div>
+            </div>
+            <div className="rounded border-2 py-8">
+              <h3>PERSONAL</h3>
+              <h1 className="text-4xl pb-10">$14/m</h1>
+              <div className="space-y-10">
+                <li>10 Projects</li>
+                <li>5 GB Storage</li>
+                <li>Basic Support</li>
+                <li>Collaboration</li>
+                <li>Reports and analytics</li>
+                <Button> CHOOSE THIS PLAN </Button>
+              </div>
+            </div>
+          </div>
+        </ul>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Pricing
+export default Pricing;
